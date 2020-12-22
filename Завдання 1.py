daa6d264a807cfd1cdbe96dada2e05e284dd9e7d
ib407ov <44349161+ib407ov@users.noreply.github.com>
2020-12-22 11:37:22 +0200

    Add files via upload

--- Завдання 1.py
+++ Завдання 1.py
@@ -1,92 +1,56 @@
-Workers1 = {"Pass": "", "Osvita": 0, "Specialnist": "", "Posada": 0, "Oklad": 0}
-arrayWorkers = []
+# cтворити змінну для тексту та назву теми
+#
+# визначити кількість букв тексту
+# визначити кількість пробілів
+# заміни кожного входження однієї букви на іншу;
+# видалення слова за номером
 
-def serch(choose, criteria):
-    if choose == 1:
-        for i in range(len(arrayWorkers)):
-            if arrayWorkers[i]["Pass"] == criteria:
-                print(arrayWorkers[i])
-    if choose == 2:
-        for i in range(len(arrayWorkers)):
-            if arrayWorkers[i]["Osvita"] == criteria:
-                print(arrayWorkers[i])
-    if choose == 3:
-        for i in range(len(arrayWorkers)):
-            if arrayWorkers[i]["Specialnist"] == criteria:
-                print(arrayWorkers[i])
-    if choose == 4:
-        for i in range(len(arrayWorkers)):
-            if arrayWorkers[i]["Posada"] == criteria:
-                print(arrayWorkers[i])
+class Text:
+    def __init__(self, _nameText, _text):
+        self.nameText = _nameText
+        self.Text = _text
 
-    if choose == 5:
-        for i in range(len(arrayWorkers)):
-            if arrayWorkers[i]["Oklad"] == criteria:
-                print(arrayWorkers[i])
+    @property
+    def howLettersInText(self):
+        return (len(self.Text))
 
+    @property
+    def howManySpaces(self):
+        a = 0
+        for i in range(g.howLettersInText):
+            if self.Text[i] == ' ':
+                a += 1
+        return a
 
-while True:
-    print("\n")
-    print("1. Вивести всю інформацію\n"
-          "2. Ввести данні about worker\n"
-          "3. Знайти workera\n"
-          "4. Вийти\n")
+    @property
+    def secretText(self):
+        l = 0
+        A = []
+        for i in range(self.howLettersInText):
+            A.append(ord(self.Text[i]))
+        return print('secret = ', A)
 
-    choose = int(input("Виберіть номер:"))
+    def deleteWord(self, numberWord):
+        A = self.Text.split()
+        print('You delete word : ', A[numberWord])
+        del A[numberWord]
+        return A
 
-    if choose == 1:
-        print()
-        for i in range(len(arrayWorkers)):
-            print(arrayWorkers[i])
-
-    elif choose == 2:
-        Dani = int(input('Pass : '))
-        Osvita = input('Osvita :')
-        Specialnit = input('Specialnist : ')
-        Posada = input('Posada : ')
-        Oklad = input('Oklad : ')
-
-        Workers1 = {"Pass": Dani, "Osvita": Osvita, "Specialnist": Specialnit, "Posada": Posada, "Oklad": Oklad}
-
-        # Workers1["Pass"] = Dani
-        # Workers1["Osvita"] = Osvita
-        # Workers1["Specialnist"] = Specialnit
-        # Workers1["Posada"] = Posada
-        # Workers1["Oklad"] = Oklad
-        arrayWorkers.append(Workers1)
-        print(Workers1)
-        print(arrayWorkers)
-
-    elif choose == 3:
-        print("Знайти за:\n"
-              "1.Паспортом\n"
-              "2.Освітою\n"
-              "3.Спеціальністю\n"
-              "4.Посала\n"
-              "5.Оклад\n")
-
-        choose2 = int(input('Введыть критерій : '))
-        if choose2 == 1:
-            characteristic = int(input())
-            serch(1, characteristic)
-
-        elif choose2 == 2:
-            characteristic = input()
-            serch(2, characteristic)
-
-        elif choose2 == 3:
-            characteristic = input()
-            serch(3, characteristic)
-
-        elif choose2 == 4:
-            characteristic = input()
-            serch(4, characteristic)
-
-        elif choose2 == 5:
-            characteristic = input()
-            serch(5, characteristic)
-
-    elif choose == 4:
-        break
-    else:
-        print('Write norlmal number\n')
+yN = input('you have write text? (y/n) : ')
+if yN == 'y':
+    a = input('Name text : ')
+    b = input('writte : ')
+    g = Text(a, b)
+else:
+    g= Text('EPIGRATH','Learn everything you need to know')
+#----------1
+print(g.howLettersInText)
+#----------2
+print(g.howManySpaces)
+#----------3
+print(g.secretText)
+#----------4
+yN = input('Do you have delete word? (y/n) : ')
+if yN == 'y':
+    a = int(input('delete word = '))
+    print(g.deleteWord(a))
--- Завдання 2.py
+++ Завдання 2.py
@@ -1,73 +1,69 @@
-# Довідник автомеханіка. База даних запчастин:
-# назва запчастини, марка автомобіля, рік випуску.
-# Організувати вибір за довільним запитом.
-# Дані зберігаються в масиві записів, який створюється динамічно.
-arreyDetails = []
-
-def serch(choose, creteria):
-    if choose == 1:
-        for i in range(len(arreyDetails)):
-            if arreyDetails[i]["назва запчастини"] == creteria:
-                print(arreyDetails[i])
-
-    elif choose == 2:
-        for i in range(len(arreyDetails)):
-            if arreyDetails[i]["марка авто"] == creteria:
-                print(arreyDetails[i])
-
-    elif choose == 3:
-        for i in range(len(arreyDetails)):
-            if arreyDetails[i]["рік випуску"] == creteria:
-                print(arreyDetails[i])
+# Виведення на екран кількості вершин;
+# виведення на екран ребер графа;
+# додавання нового ребра;
+# видалення ребра;
+# пошук шляхів між двома заданими вершинами.
 
 
 
+class Grath:
+    def __init__(self, _manyTop , _matrix):
+        self.manyTop = 6
+        self.matix = [[0, 1, 1, 0, 1, 0],
+                  [1, 0, 0, 1, 0, 0],
+                  [1, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 1, 0],
+                  [1, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0]]
 
+    @property
+    def printMatix(self):
+        for i in range(self.manyTop):
+            print(self.matix[i], end='\n')
 
+    @property
+    def printManyTop(self):
+        print('Tops = ', self.manyTop)
 
-while True:
-    choise = int(input('\n1.Вивести наявність деталей\n'
-              '2.Ввести деталі\n'
-              '3.Вивсти деталь\n'
-              '4.Закінчити\n\nВведіть число : '))
+    def addNewRib(self, x, y):
+        self.matix[x][y] = 1
+        self.matix[y][x] = 1
 
-    if choise == 1:
-        print()
-        for i in range(len(arreyDetails)):
-            print(arreyDetails[i])
+        for i in range(self.manyTop):
+            print(self.matix[i], end='\n')
 
-    elif choise == 2:
-        nameDetail = input('Деталь : ')
-        nameAuto = input('Марка авто : ')
-        year = input('Рік : ')
+    def delRib(self, x ,y):
+        self.matix[x][y] = 0
+        self.matix[y][x] = 0
 
-        details = {"назва запчастини": nameDetail, "марка авто": nameAuto, "рік випуску": year}
-        arreyDetails.append(details)
+        for i in range(self.manyTop):
+            print(self.matix[i], end='\n')
 
-    elif choise == 3:
-        choise2 = int(input('Знайти за : \n'
-              '1. Назвою\n'
-              '2. Маркою\n'
-              '3. Роком\n\n'
-              'Введіть число : '))
-        print()
+    def findingWay(self, x, y):
+        if self.matix[x][y] == 1:
+            print('The path exist!')
+        else:
+            print('Sorry. The path does not exist.')
 
-        if choise2 == 1:
-            charakteristic = input('Введіть назву: ')
-            serch(choise2, charakteristic)
+#------------------------
 
-        elif choise2 == 2:
-            charakteristic = input('Введіть марку: ')
-            serch(choise2, charakteristic)
+k = input('You have write matix? (y/n): ')
+if k == 'n':
+    mmatix = [[0, 1, 1, 0, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]]
 
+    g = Grath(6, mmatix)
+    g.findingWay(0, 1)
+else:
+    tops = int(input('Tops = '))
+    mmatix = []
+    mmatix = [[int(input("matix[{0}][{1}]=".format(i,j)))  for j in range(tops)] for i in range(tops)]
 
-        elif choise2 == 3:
-            charakteristic = input('Введіть рік: ')
-            serch(choise2, charakteristic)
-
-
-    elif choise == 4:
-        break
-
-    else:
-        print('Write normal number\n')
+t = Grath()
+t.addNewRib(5,2)
+t.delRib(5,2)
+t.findingWay(5,2)
